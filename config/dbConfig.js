f6a4e51d1f6d769f983ab002f366f07e31f33830
Aditya Kumar Dixit <96368151+adityadixit07@users.noreply.github.com>
2023-12-17 22:28:31 +0530

    build folder

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ mongoose.set("strictQuery", false);
 
 const dbConfig = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://adityadileepdixit:agqAC34yrQoxQJ5D@cluster0.dtrplyv.mongodb.net/?retryWrites=true&w=majority');
+    const conn = await mongoose.connect(`${process.env.MONGO_URL}`);
     console.log(`mongodb connected to backend successfully`);
   } catch (err) {
     console.log("error on connecting to the database");
